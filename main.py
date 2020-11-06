4e9764f68116635ab8bf434fe7034073209ceca3
tenundor@gmail.com <He5LA90m7a9T>
2020-11-06 20:06:16 +0300

    completed the seventh task from https://devman.org/encyclopedia/modules/jinja2/

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ rendered_page = template.render(
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
